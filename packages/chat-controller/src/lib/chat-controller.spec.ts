f2557b38bb462856555b65a85a14a8ac0e92a26b
Jethro Fredericks <jethrofredericks.dev@protonmail.com>
2023-05-07 14:21:23 +0200

    Initial commit

---
+++ packages/chat-controller/src/lib/chat-controller.spec.ts
@@ -0,0 +1,7 @@
+import { chatController } from './chat-controller';
+
+describe('chatController', () => {
+  it('should work', () => {
+    expect(chatController()).toEqual('chat-controller');
+  });
+});
---
+++ packages/chat-controller/src/lib/chat-controller.ts
@@ -0,0 +1,3 @@
+export function chatController(): string {
+  return 'chat-controller';
+}
