8eed1eef2ef7ff29a8bd97bdb1616dc8376cb123
Jethro Fredericks <jethrofredericks.dev@protonmail.com>
2023-05-08 15:56:21 +0200

    Add functionality for selecting peer address

--- packages/chat-app-interface/pages/home.tsx
+++ packages/chat-app-interface/pages/home.tsx
@@ -6,59 +6,76 @@ import { useForm } from 'react-hook-form';
 import { getSigner } from '../utils/signer';
 
 import Button from '../components/styled/Button';
+import Chat from '../components/Chat';
 
 export function Home() {
   const { account } = useEthers();
   const router = useRouter();
+  const [isLoading, setLoadingState] = useState(false);
   const [conversation, setConversation] = useState<Conversation>();
   const [messages, setMessages] = useState<DecodedMessage[]>([]);
+  const [peerAddress, setPeerAddress] = useState<string>();
   const {
     register,
     formState: { errors },
     handleSubmit,
   } = useForm();
   const onSubmit = async (formState: any) => {
-    try {
-      await conversation?.send(formState.message);
-    } catch (error) {
-      console.log('error');
-      console.log(error);
-    }
+    setLoadingState(true);
+    setPeerAddress(formState.peerAddress);
+
+    const signer = await getSigner();
+    const xmtp = await Client.create(signer);
+    const conversation = await xmtp.conversations.newConversation(
+      formState.peerAddress
+    );
+    const messages = await conversation.messages();
+    setConversation(conversation);
+    setMessages(messages || []);
+
+    setLoadingState(false);
   };
 
   useEffect(() => {
     if (!account) {
       router.push('/');
-    } else {
-      const connectToClient = async () => {
-        const signer = await getSigner();
-        const xmtp = await Client.create(signer);
-        const conversation = await xmtp.conversations.newConversation(
-          '0x64F8Bf6eFD1AffdE71399b0a19776336b242750D'
-        );
-        const messages = await conversation.messages();
-        setConversation(conversation);
-        setMessages(messages || []);
-      };
-
-      connectToClient();
     }
   }, []);
+
+  useEffect(() => {
+    if (peerAddress) {
+      const streamMessages = async () => {
+        const newStream = await conversation?.streamMessages();
+        for await (const msg of newStream || []) {
+          setMessages((prevMessages) => [...prevMessages, msg]);
+        }
+      };
+      streamMessages();
+    }
+  }, [conversation, peerAddress]);
+
   return (
     <div>
-      <div>
-        {messages.length === 0 && <div>No messages to display</div>}
-        {messages.length > 0 &&
-          messages.map((message) => (
-            <div key={message.id}>{message.content}</div>
-          ))}
-      </div>
-      <div>
+      {!peerAddress && !isLoading && (
         <div>
-          <input type="text" placeholder="Message" {...register('message')} />
+          <div>
+            <input
+              type="text"
+              placeholder="Peer Address"
+              {...register('peerAddress')}
+            />
+          </div>
+          <Button onClick={handleSubmit(onSubmit)}>Set Peer Address</Button>
         </div>
-        <Button onClick={handleSubmit(onSubmit)}>Send Message</Button>
-      </div>
+      )}
+      {peerAddress && isLoading && <div>Loading...</div>}
+      {peerAddress && conversation && (
+        <Chat
+          account={account as string}
+          messages={messages}
+          conversation={conversation as Conversation}
+        />
+      )}
     </div>
   );
 }
