9cd2a1d3e89a549997c4be37f1c6321c8320cbed
Jethro Fredericks <jethrofredericks.dev@protonmail.com>
2023-05-11 13:16:29 +0200

    Auto scroll to bottom on message received

--- packages/chat-app-interface/components/Messages.tsx
+++ packages/chat-app-interface/components/Messages.tsx
@@ -1,9 +1,19 @@
+import { css } from '@emotion/react';
 import { DecodedMessage } from '@xmtp/xmtp-js';
+import { useEffect } from 'react';
+import { animateScroll } from 'react-scroll';
 
 interface Props {
   messages: DecodedMessage[];
   conversationPartner: string;
 }
+
+const messagesCss = css`
+  height: 90%;
+  width: 100%;
+  overflow-y: auto;
+  padding: 10px;
+`;
 
 function Messages({ messages, conversationPartner }: Props) {
   const formatMessageTag = (senderAddress: string) => {
@@ -12,8 +22,13 @@ function Messages({ messages, conversationPartner }: Props) {
       : 'Me: ';
   };
 
+  useEffect(() => {
+    console.log('scrolling');
+    animateScroll.scrollToBottom({ containerId: 'messages' });
+  }, [messages]);
+
   return (
-    <div className="messages">
+    <div id="messages" css={messagesCss}>
       {messages.length === 0 && <div>No messages to display</div>}
       {messages.length > 0 &&
         messages.map((message) => (
